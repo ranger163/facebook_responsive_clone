e141cdf0afc7755bb4bcf86f95f0512d87a3f28e
ranger163 <ahmed.nassar163@gmail.com>
2020-08-10 19:42:01 +0200

    Creating stories.dart

---
+++ lib/widgets/stories.dart
@@ -0,0 +1,14 @@
+import 'package:facebook_responsive_clone/models/models.dart';
+import 'package:flutter/material.dart';
+
+class Stories extends StatelessWidget {
+  final User currentUser;
+  final List<Story> stories;
+
+  Stories(this.currentUser, this.stories);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
