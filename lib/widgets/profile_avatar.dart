50c45c27878a9e7728195ec1ec35df372adbf309
ranger163 <ahmed.nassar163@gmail.com>
2020-08-10 20:39:27 +0200

    Editing profile_avatar.dart

--- lib/widgets/profile_avatar.dart
+++ lib/widgets/profile_avatar.dart
@@ -5,16 +5,21 @@ import 'package:flutter/material.dart';
 class ProfileAvatar extends StatelessWidget {
   final String imageUrl;
   final bool isActive;
+  final bool hasBorder;
 
-  ProfileAvatar({@required this.imageUrl, this.isActive = false});
+  ProfileAvatar(
+      {@required this.imageUrl, this.isActive = false, this.hasBorder = false});
 
   @override
   Widget build(BuildContext context) {
     return Stack(children: [
       CircleAvatar(
         radius: 20,
-        backgroundColor: Colors.grey[200],
-        backgroundImage: CachedNetworkImageProvider(imageUrl),
+        child: CircleAvatar(
+          radius: hasBorder ? 17 : 20,
+          backgroundColor: Colors.grey[200],
+          backgroundImage: CachedNetworkImageProvider(imageUrl),
+        ),
       ),
       if (isActive)
         Positioned(
