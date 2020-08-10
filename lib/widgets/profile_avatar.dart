6b4fe61ef76ada72e14aab1c6ed13ab9501828f6
ranger163 <ahmed.nassar163@gmail.com>
2020-08-10 19:35:25 +0200

    Adding profile_avatar.dart

---
+++ lib/widgets/profile_avatar.dart
@@ -0,0 +1,36 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:facebook_responsive_clone/config/palette.dart';
+import 'package:flutter/material.dart';
+
+class ProfileAvatar extends StatelessWidget {
+  final String imageUrl;
+  final bool isActive;
+
+  ProfileAvatar({@required this.imageUrl, this.isActive = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      CircleAvatar(
+        radius: 20,
+        backgroundColor: Colors.grey[200],
+        backgroundImage: CachedNetworkImageProvider(imageUrl),
+      ),
+      if (isActive)
+        Positioned(
+            bottom: 0,
+            right: 0,
+            child: Container(
+              height: 15,
+              width: 15,
+              decoration: BoxDecoration(
+                  color: Palette.online,
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    width: 2,
+                    color: Colors.white,
+                  )),
+            ))
+    ]);
+  }
+}
