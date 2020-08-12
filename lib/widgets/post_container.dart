9783575a1145305c92a2a59c02887008a81a5dcc
ranger163 <ahmed.nassar163@gmail.com>
2020-08-12 17:08:21 +0200

    Rendering post body

--- lib/widgets/post_container.dart
+++ lib/widgets/post_container.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:facebook_responsive_clone/models/models.dart';
 import 'package:facebook_responsive_clone/widgets/profile_avatar.dart';
 import 'package:flutter/material.dart';
@@ -14,9 +15,31 @@ class PostContainer extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 8),
       color: Colors.white,
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          _PostHeader(post),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                _PostHeader(post),
+                const SizedBox(height: 4),
+                Text(post.caption),
+                const SizedBox(height: 4),
+                post.imageUrl != null
+                    ? const SizedBox.shrink()
+                    : SizedBox(
+                        height: 6,
+                      ),
+              ],
+            ),
+          ),
+          if (post.imageUrl != null)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: CachedNetworkImage(
+                imageUrl: post.imageUrl,
+              ),
+            ),
         ],
       ),
     );
