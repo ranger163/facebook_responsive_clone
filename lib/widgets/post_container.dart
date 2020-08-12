85fc857a02d9e47e7f20ebf369be310d820cf39a
ranger163 <ahmed.nassar163@gmail.com>
2020-08-12 16:55:21 +0200

    Creating and rendering post header

--- lib/widgets/post_container.dart
+++ lib/widgets/post_container.dart
@@ -1,4 +1,5 @@
 import 'package:facebook_responsive_clone/models/models.dart';
+import 'package:facebook_responsive_clone/widgets/profile_avatar.dart';
 import 'package:flutter/material.dart';
 
 class PostContainer extends StatelessWidget {
@@ -12,6 +13,61 @@ class PostContainer extends StatelessWidget {
       margin: const EdgeInsets.symmetric(vertical: 5),
       padding: const EdgeInsets.symmetric(vertical: 8),
       color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _PostHeader(post),
+        ],
+      ),
+    );
+  }
+}
+
+class _PostHeader extends StatelessWidget {
+  final Post post;
+
+  _PostHeader(this.post);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ProfileAvatar(imageUrl: post.user.imageUrl),
+        const SizedBox(
+          width: 8,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                post.user.name,
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              Row(
+                children: [
+                  Text(
+                    '${post.timeAgo} . ',
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 12,
+                    ),
+                  ),
+                  Icon(
+                    Icons.public,
+                    color: Colors.grey[600],
+                    size: 12,
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.more_horiz),
+          onPressed: () => print('More'),
+        )
+      ],
     );
   }
 }
